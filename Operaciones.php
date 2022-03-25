7f454e72a54e559a0f28a69120cf2e5f5d253d3c
Jeaneth27 <85314088+Jeaneth27@users.noreply.github.com>
2022-03-24 20:37:54 -0600

    Create Operaciones.php

---
+++ Operaciones.php
@@ -0,0 +1,21 @@
+<?php
+class Operaciones
+{
+    public function suma($a, $b)
+    {
+        return ($a+$b);
+    }
+    public function multiplicacion($a, $b)
+    {
+        return ($a*$b);
+    }
+    public function Division($a, $b)
+    {
+        return ($a/$b);
+    }
+    public function resta($a, $b)
+    {
+        return ($a-$b);
+    }
+}
+?>
